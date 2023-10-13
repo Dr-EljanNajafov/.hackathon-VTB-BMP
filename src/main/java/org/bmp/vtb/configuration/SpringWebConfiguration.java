d949543adf7bf2ec7b35f83893f84126221e5709
Eugene <genyamateyuk@gmail.com>
2023-10-13 23:00:03 +0300

    Init

---
+++ src/main/java/org/bmp/vtb/configuration/SpringWebConfiguration.java
@@ -0,0 +1,8 @@
+package org.bmp.vtb.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringWebConfiguration implements WebMvcConfigurer {
+}
